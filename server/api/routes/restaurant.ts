55aaefaf2724e12896e9eb69216b12e7c29695c9
moonhee0507 <sjmh0507@naver.com>
2023-03-06 23:57:32 +0900

    ✨ 식당정보 api 라우트 추가

---
+++ server/api/routes/restaurant.ts
@@ -0,0 +1,29 @@
+import { Router, Request, Response } from 'express';
+import Restaurant from '../../models/Restaurant.js';
+const route = Router();
+
+export default (app: Router) => {
+    app.use('/api/restaurants', route);
+
+    route.get('/', async (req: Request, res: Response) => {
+        const page = Number(req.query.page || 1);
+        const limit = Number(req.query.limit || 20);
+
+        try {
+            const total = await Restaurant.countDocuments({});
+            const lists = await Restaurant.find({})
+                .sort({ address: -1 })
+                .skip(limit * (page - 1))
+                .limit(limit);
+
+            res.json({
+                total: total,
+                countLimit: limit,
+                currentPage: page,
+                lists,
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    });
+};
