daab592c165049b060862d1c331ba0d692434a43
moonhee0507 <sjmh0507@naver.com>
2023-03-10 23:18:57 +0900

    ♻️ 서울시인증식당 정보 가져오기

--- server/api/routes/restaurant.ts
+++ server/api/routes/restaurant.ts
@@ -1,7 +1,42 @@
 import { Router, Request, Response } from 'express';
 import Restaurant from '../../models/Restaurant.js';
+import fetch from 'node-fetch';
 
 const route = Router();
+
+type FromSeoulOpenApi = {
+    CRTFC_UPSO_MGT_SNO: number;
+    UPSO_SNO: string;
+    UPSO_NM: string; // 업소명
+    CGG_CODE: string;
+    CGG_CODE_NM: string;
+    COB_CODE_NM: string;
+    BIZCND_CODE_NM: string; // 카테고리
+    OWNER_NM: string;
+    CRTFC_GBN: string;
+    CRTFC_GBN_NM: string; // 인증명
+    CRTFC_CHR_NM: string;
+    CRTFC_CHR_ID: string;
+    CRTFC_YMD: string; // 인증날짜
+    USE_YN: string;
+    MAP_INDICT_YN: string;
+    CRTFC_CLASS: string;
+    Y_DNTS: string; // 위도
+    X_CNTS: string; // 경도
+    TEL_NO: string; // 전화번호
+    RDN_DETAIL_ADDR: string;
+    RDN_ADDR_CODE: string;
+    RDN_CODE_NM: string; // 주소
+    BIZCND_CODE: string;
+    COB_CODE: string;
+    CRTFC_SNO: string;
+    CRT_TIME: string;
+    CRT_USR: string;
+    UPD_TIME: string;
+    FOOD_MENU: string;
+    GNT_NO: string;
+    CRTFC_YN: string;
+};
 
 export default (app: Router) => {
     app.use('/restaurants', route);
@@ -28,5 +63,40 @@ export default (app: Router) => {
         }
     });
 
-    route.get('/certificates', async (req: Request, res: Response) => {});
+    // 인증식당정보 저장
+    route.get('/save', async (req: Request, res: Response) => {
+        let all: Array<FromSeoulOpenApi> = [];
+        try {
+            const resForTotal = await fetch(
+                `http://openapi.seoul.go.kr:8088/68524f7775736a6d37346a78686e74/json/CrtfcUpsoInfo/1/1`
+            );
+            const data = await resForTotal.json();
+            const total = data.CrtfcUpsoInfo.list_total_count;
+
+            for (let startIndex = 1; startIndex <= total; startIndex += 1000) {
+                const resForVeganRestaurantInSeoul = await fetch(
+                    `http://openapi.seoul.go.kr:8088/68524f7775736a6d37346a78686e74/json/CrtfcUpsoInfo/${startIndex}/${
+                        startIndex + 999
+                    }`
+                );
+                const veganRestaurantInSeoul = await resForVeganRestaurantInSeoul.json();
+
+                all.push(
+                    ...veganRestaurantInSeoul.CrtfcUpsoInfo.row.filter(
+                        (item: any) => item.CRTFC_GBN_NM === '채식가능음식점' || item.CRTFC_GBN_NM === '채식음식점'
+                    )
+                );
+
+                if (startIndex === 1000 * Math.trunc(total / 1000) + 1) {
+                    res.json({
+                        all,
+                    });
+                }
+            }
+        } catch (err) {
+            console.error(err);
+        }
+
+        // const restaurant = new Restaurant(req.body);
+    });
 };
