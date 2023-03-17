41e4c2c4c47c3c81b36a0c7bcdbde312cd3788cd
moonhee0507 <sjmh0507@naver.com>
2023-03-17 23:32:18 +0900

    ✏️ Jest 환경 세팅

--- test/kakaoMapAPI.test.ts
+++ test/kakaoMapAPI.test.ts
@@ -1,4 +1,4 @@
-import { codeToName } from './kakaoMapAPI.js';
+import { codeToName } from '../pages/search/kakaoMapAPI';
 
 test('주소코드 변환', () => {
     expect(codeToName('서울특별시 3000000 능동 135')).toBe('서울특별시 종로구 능동 135');
