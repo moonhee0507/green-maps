c2634232b68f38b5718245ba695ffd7aef7d117b
moonhee0507 <sjmh0507@naver.com>
2023-04-17 14:53:52 +0900

    :see_no_evil: .gitignore 수정

---
+++ assets/chunks/1aea29e6.js
@@ -0,0 +1,1 @@
+import{j as o}from"./0e184ced.js";const n="/images/icon-arrow.svg";function c(){function t(){window.history.back()}return o("button",{className:"button-back",onClick:t,children:o("img",{src:n,alt:"뒤로가기"})})}export{c as B};
