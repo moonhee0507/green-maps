455ba6576906e3db5da27dda4f26ee97cfb7a32b
moonhee0507 <sjmh0507@naver.com>
2023-04-17 16:46:47 +0900

    ✏️ github actions 테스트4

--- pages/index/homeContent.tsx
+++ pages/index/homeContent.tsx
@@ -3,43 +3,5 @@ import React from 'react';
 export { HomeContent };
 
 function HomeContent() {
-    return (
-        <div className="home-content">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi saepe optio tempore animi a, odio, illo
-            corporis deleniti ratione recusandae quia laudantium eaque nesciunt harum totam ipsa, repellat voluptate.
-            Nobis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi saepe optio tempore animi a, odio,
-            illo corporis deleniti ratione recusandae quia laudantium eaque nesciunt harum totam ipsa, repellat
-            voluptate. Nobis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi saepe optio tempore
-            animi a, odio, illo corporis deleniti ratione recusandae quia laudantium eaque nesciunt harum totam ipsa,
-            repellat voluptate. Nobis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores nobis aliquam
-            ut ipsum quidem, consectetur deleniti at dolorem accusamus numquam sapiente magnam consequatur! Aperiam
-            tempora, deleniti porro aliquid eius quam! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Voluptatum dolore culpa obcaecati eos, ipsum harum dicta eaque quod reiciendis assumenda, facilis natus
-            quisquam animi optio perferendis omnis non iste doloribus. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Rem impedit vel sint saepe labore ab modi magnam, ad illum esse qui illo! Inventore
-            facilis necessitatibus aliquam ex eos quas quibusdam. Lorem ipsum, dolor sit amet consectetur adipisicing
-            elit. Aliquam recusandae tempore blanditiis quaerat, iusto, debitis minus saepe veritatis asperiores
-            eligendi perspiciatis neque reprehenderit distinctio quod nisi voluptatum, odit quas repudiandae! Lorem
-            ipsum dolor, sit amet consectetur adipisicing elit. Placeat debitis facilis nostrum iusto quod accusamus
-            ipsum sequi atque repellendus natus optio porro voluptate, mollitia consectetur suscipit, dignissimos nam
-            doloremque nesciunt! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi saepe optio tempore
-            animi a, odio, illo corporis deleniti ratione recusandae quia laudantium eaque nesciunt harum totam ipsa,
-            repellat voluptate. Nobis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi saepe optio
-            tempore animi a, odio, illo corporis deleniti ratione recusandae quia laudantium eaque nesciunt harum totam
-            ipsa, repellat voluptate. Nobis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi saepe
-            optio tempore animi a, odio, illo corporis deleniti ratione recusandae quia laudantium eaque nesciunt harum
-            totam ipsa, repellat voluptate. Nobis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
-            nobis aliquam ut ipsum quidem, consectetur deleniti at dolorem accusamus numquam sapiente magnam
-            consequatur! Aperiam tempora, deleniti porro aliquid eius quam! Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Voluptatum dolore culpa obcaecati eos, ipsum harum dicta eaque quod reiciendis assumenda,
-            facilis natus quisquam animi optio perferendis omnis non iste doloribus. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Rem impedit vel sint saepe labore ab modi magnam, ad illum esse qui illo!
-            Inventore facilis necessitatibus aliquam ex eos quas quibusdam. Lorem ipsum, dolor sit amet consectetur
-            adipisicing elit. Aliquam recusandae tempore blanditiis quaerat, iusto, debitis minus saepe veritatis
-            asperiores eligendi perspiciatis neque reprehenderit distinctio quod nisi voluptatum, odit quas repudiandae!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat debitis facilis nostrum iusto quod
-            accusamus ipsum sequi atque repellendus natus optio porro voluptate, mollitia consectetur suscipit,
-            dignissimos nam doloremque nesciunt!
-        </div>
-    );
+    return <div className="home-content">main page</div>;
 }
