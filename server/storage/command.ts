97bd611eedb2f5b20dd2d65b6f203b49cc9b85a5
moonhee0507 <sjmh0507@naver.com>
2023-04-29 14:39:00 +0900

    :adhesive_bandage: S3Client command 임시 저장

--- server/storage/command.ts
+++ server/storage/command.ts
@@ -1,5 +1,4 @@
-import { PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
-import { Upload } from '@aws-sdk/lib-storage';
+import { GetBucketAclCommand, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import auth from './config.js';
 
