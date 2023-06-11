ebea88a8d0c423eac37d16590e415d802c213237
moonhee0507 <sjmh0507@naver.com>
2023-06-12 06:02:51 +0900

    :truck: vite-plugin-vercel 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react';
 import ssr from 'vite-plugin-ssr/plugin';
+import vercel from 'vite-plugin-vercel';
+import vercelSsr from '@magne4000/vite-plugin-vercel-ssr';
 import { defineConfig, UserConfig } from 'vite';
 
 export default defineConfig(async ({ command, mode }) => {
     return {
-        plugins: [react(), ssr({ prerender: false })],
+        plugins: [react(), ssr({ prerender: false }), vercel(), vercelSsr()],
         build: {
             manifest: true,
         },
