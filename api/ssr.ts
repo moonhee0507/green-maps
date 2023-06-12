f5760ea7e77f50e637a4e8771fa6263bf19aabf9
moonhee0507 <sjmh0507@naver.com>
2023-06-12 20:27:38 +0900

    :truck: ssr.ts에 allowCors 함수 이동

--- api/ssr.ts
+++ api/ssr.ts
@@ -4,7 +4,23 @@ import { Request, Response } from 'express';
 /**
  * 서버리스 함수: 하드웨어 설정 없이 온디맨드 방식으로 코드 실행
  */
-export default async function handler(req: Request, res: Response) {
+const allowCors = (fn: Function) => async (req: Request, res: Response) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', 'https://green-maps-git-preview-moonhee0507.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+    return await fn(req, res);
+};
+
+async function handler(req: Request, res: Response) {
     const { url } = req;
     console.log('Request to url:', url);
 
@@ -23,3 +39,5 @@ export default async function handler(req: Request, res: Response) {
     res.setHeader('content-type', contentType);
     res.end(body);
 }
+
+export default allowCors(handler);
