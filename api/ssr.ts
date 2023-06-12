665e8fc1c87bf96550c2bb6aa502141de7072a7e
moonhee0507 <sjmh0507@naver.com>
2023-06-12 20:39:55 +0900

    :bug: handler setHeader 수정

--- api/ssr.ts
+++ api/ssr.ts
@@ -4,23 +4,7 @@ import { Request, Response } from 'express';
 /**
  * 서버리스 함수: 하드웨어 설정 없이 온디맨드 방식으로 코드 실행
  */
-const allowCors = (fn: Function) => async (req: Request, res: Response) => {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', 'https://green-maps-git-preview-moonhee0507.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-    return await fn(req, res);
-};
-
-async function handler(req: Request, res: Response) {
+export default async function handler(req: Request, res: Response) {
     const { url } = req;
     console.log('Request to url:', url);
 
@@ -34,10 +18,21 @@ async function handler(req: Request, res: Response) {
         return;
     }
 
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     const { body, statusCode, contentType } = httpResponse;
     res.statusCode = statusCode;
-    res.setHeader('content-type', contentType);
+    res.setHeader('Content-Type', contentType);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', 'https://green-maps-git-preview-moonhee0507.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
     res.end(body);
 }
-
-export default allowCors(handler);
