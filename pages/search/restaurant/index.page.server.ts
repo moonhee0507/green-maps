0ecab0edf4bba02fd9b9fdb4e99cb1fa0d0785f3
moonhee0507 <sjmh0507@naver.com>
2023-06-14 01:51:40 +0900

    :recycle: /search/:restaurantId 페이지 레이지로딩

--- pages/search/restaurant/index.page.server.ts
+++ pages/search/restaurant/index.page.server.ts
@@ -1,41 +1,14 @@
-import fetch from 'node-fetch';
-import { API_URL } from '../../../renderer/CONSTANT_URL';
 import type { PageContext } from '../../../renderer/types';
-import type { Restaurant } from '../../../server/models/Restaurant';
-
-type Params = {
-    restaurantId: string;
-};
 
 export { onBeforeRender };
 
 async function onBeforeRender(pageContext: PageContext) {
-    const { restaurantId } = pageContext.routeParams as Params;
-    console.log('레스토랑 pageContext', pageContext);
-    console.log('pageContext.routeParams', pageContext.routeParams);
+    const { routeParams, user } = pageContext;
 
-    const { user } = pageContext;
-
-    const res = await fetch(`${API_URL}/restaurants/${restaurantId}`, {
-        headers: {
-            'Cache-Control': 'max-age=31536000',
+    return {
+        pageContext: {
+            user,
+            routeParams,
         },
-    });
-    const data = (await res.json()) as { success: boolean; restaurantInfo: Restaurant; totalReview: number };
-
-    if (data.success) {
-        return {
-            pageContext: {
-                restaurantInfo: data.restaurantInfo,
-                user: user,
-            },
-        };
-    } else {
-        return {
-            pageContext: {
-                restaurantInfo: {},
-                user: user,
-            },
-        };
-    }
+    };
 }
