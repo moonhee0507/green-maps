35749b5fad632dd562fb7116ec5c3dcdbd947dc3
moonhee0507 <sjmh0507@naver.com>
2023-06-16 08:45:49 +0900

    :bug: store 확인

--- pages/community/component/PostSection/PostList.tsx
+++ pages/community/component/PostSection/PostList.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PostListItem from './PostListItem';
 import useIntersectionObserver from '../../useIntersectionObserver';
 import { useDispatch, useStore } from 'react-redux';
 import type { Post } from '../../../../server/models/Post';
+import { useAppSelector } from '../../../../renderer/store/hooks';
 
 export { PostList };
 
@@ -10,27 +11,35 @@ function PostList(props: { posts: Array<Post>; limit: number }) {
     const { posts, limit } = props;
     const target = useRef<HTMLLIElement>(null);
 
-    const store = useStore<any>();
+    // const store = useStore<any>();
 
     const dispatch = useDispatch();
 
+    const [totalCount, currentPage] = useAppSelector((state) => {
+        return [state.postSlice.post.TOTAL, state.postSlice.post.CURRENT_PAGE];
+    });
+
     const [observe, unobserve] = useIntersectionObserver(() => {
         // 현재 페이지가 마지막 페이지가 아닐 때만 페이지 증가
-        const lastPage = Math.ceil(store.getState().postSlice.post.TOTAL / limit);
-        const currentPage = store.getState().postSlice.post.CURRENT_PAGE;
+        // const lastPage = Math.ceil(store.getState().postSlice.post.TOTAL / limit);
+        // const currentPage = store.getState().postSlice.post.CURRENT_PAGE;
+        const lastPage = Math.ceil(totalCount / limit);
 
         if (currentPage < lastPage) {
             dispatch({
                 type: 'postSlice/POST_IN_PAGE',
-                TOTAL: store.getState().postSlice.post.TOTAL,
-                CURRENT_PAGE: store.getState().postSlice.post.CURRENT_PAGE + 1,
+                // TOTAL: store.getState().postSlice.post.TOTAL,
+                // CURRENT_PAGE: store.getState().postSlice.post.CURRENT_PAGE + 1,
+                TOTAL: totalCount,
+                CURRENT_PAGE: currentPage + 1,
             });
         }
     });
 
     useEffect(() => {
-        const lastPage = Math.ceil(store.getState().postSlice.post.TOTAL / limit);
-        const currentPage = store.getState().postSlice.post.CURRENT_PAGE;
+        // const lastPage = Math.ceil(store.getState().postSlice.post.TOTAL / limit);
+        // const currentPage = store.getState().postSlice.post.CURRENT_PAGE;
+        const lastPage = Math.ceil(totalCount / limit);
 
         if (currentPage === lastPage) {
             unobserve(target.current);
@@ -39,7 +48,7 @@ function PostList(props: { posts: Array<Post>; limit: number }) {
             observe(target.current);
             console.log(`currentPage가 ${currentPage}이고 lastPage가 ${lastPage}여서 감시 계속 =======`);
         }
-    }, [observe, unobserve, target]);
+    }, [observe, unobserve, totalCount, currentPage]);
 
     return (
         <ul className="wrapper-posts">
