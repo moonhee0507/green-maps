4ccbf3cd6c2aae97df7f04acd775a299510ee947
moonhee0507 <sjmh0507@naver.com>
2023-06-22 21:49:50 +0900

    :pencil: google webmaster

--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -76,6 +76,7 @@ async function render(pageContext: PageContextServer) {
             <meta property="twitter:title" content="Green Maps">
             <meta property="twitter:description" content="${desc}">
             <meta name="naver-site-verification" content="a3db4e78827a23b8b0d328ce26c6c74abb4d4a3a" />
+            <meta name="google-site-verification" content="BPU_wLaUb66NU-RLN8MSZzYqXkrTdN67ExNTPROn1YY" />
         </head>
         <body>
             <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=136def8e37bfc98bffe8939cd80ab687&libraries=services,clusterer,drawing"></script>
