7209be09b412d6ca9c56c7464d23ae053c9ece69
moonhee0507 <sjmh0507@naver.com>
2023-06-23 00:27:59 +0900

    :bug: textarea

--- pages/community/post/ContentSection/TextArea/TextArea.tsx
+++ pages/community/post/ContentSection/TextArea/TextArea.tsx
@@ -1,25 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import DOMPurify from 'isomorphic-dompurify';
 
 export { TextArea };
 
 function TextArea(props: { content: string }) {
-    const [hasWindow, setHasWindow] = useState(false);
-
     const htmlString = props.content;
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            setHasWindow(true);
-        } else setHasWindow(false);
-    }, []);
-
-    return hasWindow ? (
+    return typeof window !== 'undefined' ? (
         <div
             dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(htmlString) }}
             style={{ wordBreak: 'keep-all', marginTop: '40px' }}
         />
     ) : (
-        <div>Load...</div>
+        <div style={{ wordBreak: 'keep-all', marginTop: '40px' }} />
     );
 }
