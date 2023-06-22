b613abf167483e0663cfd08e89ea2fd10b018c71
moonhee0507 <sjmh0507@naver.com>
2023-06-23 00:12:41 +0900

    :bug: dompurify

--- pages/community/post/ContentSection/TextArea/TextArea.tsx
+++ pages/community/post/ContentSection/TextArea/TextArea.tsx
@@ -1,44 +1,25 @@
-// import React from 'react';
-// import DOMPurify from 'isomorphic-dompurify';
-
-// export { TextArea };
-
-// function TextArea(props: { content: string }) {
-//     const htmlString = props.content;
-
-//     return (
-//         <div
-//             dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(htmlString) }}
-//             style={{ wordBreak: 'keep-all', marginTop: '40px' }}
-//         />
-//     );
-// }
-
 import React, { useEffect, useState } from 'react';
+import DOMPurify from 'isomorphic-dompurify';
 
 export { TextArea };
 
 function TextArea(props: { content: string }) {
-    const [DOMPurify, setDOMPurify] = useState<any | null>(null);
-
-    useEffect(() => {
-        // 비동기적으로 DOMPurify 모듈을 가져옴
-        import('isomorphic-dompurify').then((module) => {
-            setDOMPurify(module.default);
-        });
-    }, []);
-
-    if (!DOMPurify) {
-        // DOMPurify가 아직 로드되지 않았을 경우 로딩 상태 또는 대체 컨텐츠를 반환할 수 있음
-        return <div>Loading...</div>;
-    }
+    const [hasWindow, setHasWindow] = useState(false);
 
     const htmlString = props.content;
 
-    return (
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setHasWindow(true);
+        } else setHasWindow(false);
+    }, []);
+
+    return hasWindow ? (
         <div
             dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(htmlString) }}
             style={{ wordBreak: 'keep-all', marginTop: '40px' }}
         />
+    ) : (
+        <div>Load...</div>
     );
 }
