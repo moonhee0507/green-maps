71d0aba678248a7ba8c3f918b87e01bdd6002a6f
moonhee0507 <sjmh0507@naver.com>
2023-09-24 00:30:34 +0900

    :recycle: 식당검색 api 미들웨어 모듈화

--- server/middleware/setSearchRestaurantQuery.ts
+++ server/middleware/setSearchRestaurantQuery.ts
@@ -1,8 +1,13 @@
 import { Response, NextFunction } from "express";
-import { PostSearchReq } from "../api/types";
+import { PostSearchReq, SearchOrder } from "../api/types.js";
 import CATEGORIES from "../../components/image/CATEGORY.js";
 
-export { setSearchCategoryQuery, setSearchCertQuery };
+export { 
+    setSearchCategoryQuery, 
+    setSearchCertQuery, 
+    setSearchSortQuery, 
+    setSearchDistanceQuery 
+};
 
 function setSearchCategoryQuery(req: PostSearchReq, res: Response, next: NextFunction) {
     const category = req.body.category;
@@ -53,6 +58,70 @@ function setSearchCertQuery(req: PostSearchReq, res: Response, next: NextFunctio
         req.certQuery = {
             $match: {},
         }
+    }
+
+    next();
+}
+
+function setSearchSortQuery(req: PostSearchReq, res: Response, next: NextFunction) {
+    const orderBy = req.query.orderBy ?? SearchOrder.RELEVANCE;
+
+    if (orderBy === SearchOrder.RATING) {
+        req.sortQuery = {
+            $sort: {
+                rating: -1,
+                _id: 1,
+                countReview: 1,
+                distance: 1,
+                accuracy: 1
+            },
+        }
+    } else if (orderBy === SearchOrder.REVIEW) { // countReview 필드 추가
+        req.sortQuery = {
+            $sort: {
+                countReview: -1,
+                _id: 1,
+                rating: 1,
+                distance: 1,
+                accuracy: 1
+            },
+        }
+    } else if (orderBy === SearchOrder.DISTANCE) {
+        req.sortQuery = {
+            $sort: {
+                distance: 1,
+                _id: 1,
+                rating: 1,
+                countReview: 1,
+                accuracy: 1
+            },
+        };
+    } else {
+        req.sortQuery = {
+            $sort: {
+                accuracy: -1,
+                _id: 1,
+                rating: 1,
+                countReview: 1,
+                distance: 1
+            },
+        };
+    }
+
+    next();
+}
+
+function setSearchDistanceQuery(req: PostSearchReq, res: Response, next: NextFunction) {
+    const currentLocation = req.body.currentLocation;
+    req.distanceQuery = {
+        $geoNear: {
+            spherical: true,
+            near: {
+                type: 'Point',
+                coordinates: currentLocation,
+            },
+            distanceField: 'distance',
+        },
     }
 
     next();
