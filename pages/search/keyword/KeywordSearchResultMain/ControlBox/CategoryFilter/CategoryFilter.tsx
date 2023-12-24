193644b21e9479af3ac71dad90b089ba84f173bf
moonhee0507 <sjmh0507@naver.com>
2023-12-24 17:17:45 +0900

    :bug: 카테고리 적용 후 다시 필터를 적용할 때 기존에 선택한 카테고리만 선택/해제되는 버그 수정

--- pages/search/keyword/KeywordSearchResultMain/ControlBox/CategoryFilter/CategoryFilter.tsx
+++ pages/search/keyword/KeywordSearchResultMain/ControlBox/CategoryFilter/CategoryFilter.tsx
@@ -1,39 +1,39 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useAppDispatch } from '../../../../../../renderer/store/hooks';
-import CATEGORIES from '../../../../../../components/image/CATEGORY';
 import { ADD_SELECTED_CATEGORY } from '../../../../../../renderer/_reducers/_slices/mapSlice';
-import { CategoryItem } from '../../../../ModalGroup/CategoryFilterModal/CategoryFilterModal';
+import { userCategory } from '../../../../ModalGroup/CategoryFilterModal/CategoryFilterModal';
 import { ApplyButton } from './ApplyButton';
+import { CheckAllButton, UncheckAllButton } from '../../../../../../components/button/CheckboxHandlerButton';
+import CategoryItem from '../../../../../../components/checkbox/CategoryItem';
 
 export { CategoryFilter };
 
 function CategoryFilter() {
     const dispatch = useAppDispatch();
-
-    const [categoryList, _] = useState<string[]>(() => {
-        const tempList = Object.keys(CATEGORIES).filter((key) => key !== '기타'); // [일식, ... ]
-
-        dispatch(ADD_SELECTED_CATEGORY([...tempList].sort()));
-
-        return [...tempList].sort(); // 체크박스 렌더링용
-    });
-
     const checkboxRefs = useRef<Array<React.RefObject<HTMLInputElement>>>([]);
 
     useEffect(() => {
-        // current에 배열을 할당(ref 배열)
-        checkboxRefs.current = Array(categoryList.length)
+        dispatch(ADD_SELECTED_CATEGORY('*'));
+
+        checkboxRefs.current = Array(userCategory.length)
             .fill(null)
             .map(() => React.createRef<HTMLInputElement>());
-    }, [categoryList]);
+    }, []);
 
-    const handleUncheck = () => {
-        // current 배열을 순회하면서 check 끔
-        checkboxRefs.current.map((ref) => {
-            if (ref.current !== null) {
-                ref.current.checked = false;
-            }
-        });
+    const handleAllCheckbox = ({ on }: { on: boolean }) => {
+        if (on) {
+            checkboxRefs.current.map((ref) => {
+                if (ref.current) {
+                    ref.current.checked = true;
+                }
+            });
+        } else {
+            checkboxRefs.current.map((ref) => {
+                if (ref.current) {
+                    ref.current.checked = false;
+                }
+            });
+        }
     };
 
     return (
@@ -43,13 +43,11 @@ function CategoryFilter() {
                 <form>
                     <ApplyButton />
                     <div className="container-button-all">
-                        <button type="button" onClick={handleUncheck}>
-                            전체 해제
-                        </button>
-                        <button type="reset">전체 선택</button>
+                        <UncheckAllButton handler={() => handleAllCheckbox({ on: false })} />
+                        <CheckAllButton handler={() => handleAllCheckbox({ on: true })} />
                     </div>
                     <div className="wrapper-checkbox-category reuse-in-result">
-                        {categoryList.map((name, i) => {
+                        {userCategory.map((name, i) => {
                             return (
                                 <CategoryItem key={Math.random()} name={name} index={i} ref={checkboxRefs.current[i]} />
                             );
